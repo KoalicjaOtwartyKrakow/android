45523fdcbc88c91af43157d62204296b211ce334
OleksandraP <petrovskayaas@gmail.com>
2022-02-25 21:54:25 +0100

    getWorks

--- app/src/main/java/com/example/apartments/ApartmentService.java
+++ app/src/main/java/com/example/apartments/ApartmentService.java
@@ -3,10 +3,15 @@ package com.example.apartments;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 
 public interface ApartmentService {
-    @GET("/apartments")
+    @GET("/dev/apartments")
     Call<List<ApartmentDTO>> listApartments();
+
+    @POST("/dev/apartments")
+    Call<ApartmentDTO> postAnApartment(@Body ApartmentDTO apartmentDTO);
 }
--- app/src/main/java/com/example/apartments/OfferApartmentActivity.kt
+++ app/src/main/java/com/example/apartments/OfferApartmentActivity.kt
@@ -1,23 +1,50 @@
 package com.example.apartments
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 class OfferApartmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_offer_apartment)
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://cu2kg3w6c1.execute-api.eu-west-1.amazonaws.com/dev/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        val button: Button = findViewById(R.id.button_id)
+        val textView: TextView = findViewById(R.id.text_view_id)
+        button.setOnClickListener {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://cu2kg3w6c1.execute-api.eu-west-1.amazonaws.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
 
-        val service: ApartmentService = retrofit.create(ApartmentService::class.java)
+            val service: ApartmentService = retrofit.create(ApartmentService::class.java)
 
-        val apartmentsList: Call<List<ApartmentDTO>> = service.listApartments()
+            val apartmentsListCall: Call<List<ApartmentDTO>> = service.listApartments()
+            apartmentsListCall.enqueue(object : Callback<List<ApartmentDTO>> {
+
+                override fun onResponse(
+                    call: Call<List<ApartmentDTO>>,
+                    response: Response<List<ApartmentDTO>>
+                ) {
+                    textView.setText(response.body().toString())
+                }
+
+                override fun onFailure(call: Call<List<ApartmentDTO>>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+            })
+            // val apartmentsList: Call<ApartmentDTO> = service.postAnApartment(ApartmentDTO())
+            // textView.setText(apartmentList.toString())
+        }
+
+
     }
 }
