5ff9d5d185786a5913722af0db781b5c9aad8d6d
OleksandraP <petrovskayaas@gmail.com>
2022-02-25 22:34:00 +0100

    commit

--- app/src/main/java/com/example/apartments/OfferApartmentActivity.kt
+++ app/src/main/java/com/example/apartments/OfferApartmentActivity.kt
@@ -19,6 +19,8 @@ class OfferApartmentActivity : AppCompatActivity() {
 
         val button: Button = findViewById(R.id.button_id)
         val textView: TextView = findViewById(R.id.text_view_id)
+        val landlordName: TextView = findViewById(R.id.landlord_name)
+        val landlordStreet: TextView = findViewById(R.id.landlord_street)
         button.setOnClickListener {
             val retrofit = Retrofit.Builder()
                 .baseUrl("https://cu2kg3w6c1.execute-api.eu-west-1.amazonaws.com/")
@@ -28,23 +30,30 @@ class OfferApartmentActivity : AppCompatActivity() {
             val service: ApartmentService = retrofit.create(ApartmentService::class.java)
 
             val apartmentsListCall: Call<List<ApartmentDTO>> = service.listApartments()
-            apartmentsListCall.enqueue(object : Callback<List<ApartmentDTO>> {
-
-                override fun onResponse(
-                    call: Call<List<ApartmentDTO>>,
-                    response: Response<List<ApartmentDTO>>
-                ) {
-                    textView.setText(response.body().toString())
-                }
-
-                override fun onFailure(call: Call<List<ApartmentDTO>>, t: Throwable) {
-                    TODO("Not yet implemented")
-                }
-            })
+            getApartmentsList(apartmentsListCall, textView)
             // val apartmentsList: Call<ApartmentDTO> = service.postAnApartment(ApartmentDTO())
             // textView.setText(apartmentList.toString())
         }
 
 
+    }
+
+    private fun getApartmentsList(
+        apartmentsListCall: Call<List<ApartmentDTO>>,
+        textView: TextView
+    ) {
+        apartmentsListCall.enqueue(object : Callback<List<ApartmentDTO>> {
+
+            override fun onResponse(
+                call: Call<List<ApartmentDTO>>,
+                response: Response<List<ApartmentDTO>>
+            ) {
+                textView.setText(response.body().toString())
+            }
+
+            override fun onFailure(call: Call<List<ApartmentDTO>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
